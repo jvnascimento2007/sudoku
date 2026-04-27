cdedaa2fd328baf06241a848e0626eab2df5395b
João Victor <pessoal.jv.nascimento@gmail.com>
2026-04-27 16:07:34 -0300

    adiciona: funcionalidade das celulas do jogo

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style/style.css'
 
 // elementos html
 const digitsBar = document.getElementById('digits')
+const sudokuGame = document.getElementById('sudoku')
 
 // variaveis de jogo
 var selectedDigit = '-'
@@ -9,6 +10,7 @@ var selectedDigit = '-'
 // iniciar jogo
 function init() {
 
+    // inicializa os digitos selecionaveis
     for(var i = 1; i <= 9; i++) {
 
         const digit = document.createElement('span')
@@ -22,6 +24,24 @@ function init() {
         digit.addEventListener('click', digitClicked)
 
         digitsBar.appendChild(digit)
+
+    }
+
+    // inicializa as celulas do sudoku
+    for(var c = 0; c < 9; c++) {
+
+        for(var r = 0; r < 9; r++) {
+
+            const cell = document.createElement('span')
+
+            cell.classList.add('cell')
+            cell.id = `${c}-${r}`
+
+            cell.addEventListener('click', cellClicked)
+
+            sudokuGame.appendChild(cell)
+
+        }
 
     }
 
@@ -42,6 +62,17 @@ function digitClicked() {
     selectedDigit = digitValue
 
     this.classList.add('pressed-digit')
+
+}
+
+// clique de uma célula
+function cellClicked(event) {
+
+    if(selectedDigit == '-') return;
+
+    const cell = event.target
+
+    cell.innerHTML = selectedDigit
 
 }
 
